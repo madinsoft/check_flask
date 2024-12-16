ce3b60eb092369351d8a8084a878347efc839915
madinsoft <madinsoft@gmail.com>
2024-12-16 16:08:03 +0000

    test reload

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def hello():
     except Exception as error:
         print(f"Erreur lors de la connexion à PostgreSQL : {error}")
 
-    return f"Hello World! DB Version : {db_version}"
+    return f"Test new container : Hello World! DB Version : {db_version}"
 
 if __name__ == '__main__':
     port = getenv('FLASK_PORT') or 8080
